592d3f543ed45eaea012fa96b92bd9ca0c68f57a
Amit Srivastava <46101894+cynicphoenix@users.noreply.github.com>
2019-04-23 00:59:55 +0530

    Correct fibonacci code

--- sample risc v codes/rec_fibo.asm
+++ sample risc v codes/rec_fibo.asm
@@ -1,24 +1,26 @@
-.text
-addi x3, x0, 4
+addi x3, x0, 10
 addi x8, x0, 1
-jal x1,fib
-beq x0,x0,exit
-
-fib: bgt x3,x8,l1
+jal x1,fib 
+beq x0,x0,exit 
+fib:blt x8,x3,l1 
     addi x4,x3,0
-    jalr x0,0(x1) #return
+    jalr x0, 0(x1) 
 l1: addi x2,x2,-12
-    sw x1,0(x2)#save return address
-    sw x3,4(x2)#save n
-    addi x3,x3,-1#n-1
-    jal x1,fib
-    lw x3,4(x2)#restore n
-    sw x4,8(x2)#save return value from fib(n-1)
+    sw x1,0(x2)
+    sw x3,4(x2)
+    addi x3,x3,-1
+    jal x1,fib 
+    lw x3,4(x2)
+    sw x4,8(x2)
+    add x0, x0, x0
     addi x3,x3,-2
-    jal x1,fib
-    lw x5,8(x2)#restore return value from fib(n-1)
-    add x4,x4,x5#our return value
+    jal x1,fib 
+    lw x5,8(x2)
+    add x0, x0, x0
+    add x0, x0, x0
+    add x4,x4,x5
     lw x1,0(x2)
+    add x0, x0, x0
     addi x2,x2,12
-    jalr x0,0(x1) #return
+    jalr x0, 0(x1) 
 exit:
